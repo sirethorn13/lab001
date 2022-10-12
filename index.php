8987398c2d78c7d956138f23c903bb626fe63e88
sirethorn13 <113495374+sirethorn13@users.noreply.github.com>
2022-10-12 10:11:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,5 +9,6 @@
         <h1><?php echo 'Hello, World!'; ?></h1>
         <h1><?php echo 'sirethorn-58010916062'; ?></h1>
         <h1><?php echo 'resume'; ?></h1>
+        <h1><?php echo 'ชื่อ นางสาส สิรีธร ใจเอื้อเฟื้อ'; ?></h1>
 </body>
 </html>
